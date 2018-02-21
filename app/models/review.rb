34efbcf516f5c1656d105a39fbee47c01da6beb9
Asma Laaribi <laaribi.asm@gmail.com>
2018-02-21 16:32:27 -0500

    completed the review and restaurant controllers as well as the needed views

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,5 @@ class Review < ApplicationRecord
 
   validates :restaurant, presence: true
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { :in [0, 1, 2, 3, 4, 5] }
-
+  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurant_attributes = [
+  {
+    name: "Gino's",
+    address: "rachel street",
+    phone_number: "514-111-1111",
+    category: "italian"
+  },
+  {
+    name: "Modavie",
+    address: "saint paul street",
+    phone_number: "514-222-2222",
+    category: "french"
+  },
+  {
+    name: "Lucca",
+    address: "Dante street",
+    phone_number: "514-333-3333",
+    category: "italian"
+  },
+  {
+    name: "Chez Chen",
+    address: "Lincoln street",
+    phone_number: "514-444-4444",
+    category: "chinese"
+  },
+  {
+    name: "Kazu",
+    address: "saint catherine street",
+    phone_number: "514-555-5555",
+    category: "japanese"
+  }
+]
+
+Restaurant.create!(restaurant_attributes)
+puts 'Finished'
